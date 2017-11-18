7fa8955a35df024477e25fbc532c858556f9152c
Israelb <yb8200@gmail.com>
2017-11-18 23:25:36 +0200

    add index and js files

---
+++ src/menuapp/menudata.service.js
@@ -0,0 +1,10 @@
+(function () {
+  'use strict';
+  angular.module('data')
+  .service('MenuDataService', MenuDataService);
+
+  MenuDataService.$inject = ['$q']
+  function MenuDataService($q) {
+
+  }
+})();
